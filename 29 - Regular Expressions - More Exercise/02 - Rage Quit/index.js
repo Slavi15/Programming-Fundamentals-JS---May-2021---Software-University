3115861def819ef8e6f1ea3e10a3051e52262601
Slavi15 <slavirezashkiofficial@gmail.com>
2023-06-15 20:02:11 +0300

    Update 29 - Regular Expressions - More Exercise

--- 29 - Regular Expressions - More Exercise/02 - Rage Quit/index.js
+++ 29 - Regular Expressions - More Exercise/02 - Rage Quit/index.js
@@ -1,12 +1,11 @@
 function rageQuit(string) {
-    console.log(string);
-    const regex = /[0-9]/g;
+    const regex = /[0-9]+/g;
 
     let uniqueLetters = [];
     let output = [];
 
-    const letters = string.split(regex).filter(n => n);
-    const numbers = string.match(regex).map(Number);
+    const letters = string[0].split(regex).filter(n => n);
+    const numbers = string[0].match(regex).map(Number);
 
     const lettersString = letters.join('');
     for (let i = 0; i < lettersString.length; i++) {
@@ -21,5 +20,6 @@ function rageQuit(string) {
     console.log(output.join(''));
 };
 
-rageQuit('a3');
-rageQuit('aSd2&5s@1');
+rageQuit(['a3']);
+rageQuit(['aSd2&5s@1']);
+rageQuit(['e-!btI17z=E:DMJ19U1Tvg VQ>11P"qCmo.-0YHYu~o%/%b.}a[=d15fz^"{0^/pg.Ft{W12`aD<l&$W&)*yF1WLV9_GmTf(d0($!$`e/{D\'xi]-~17 *%p"%|N>zq@ %xBD18<Y(fHh`@gu#Z#p"Z<v13fI]\':\Iz.17*W:\mwV`z-15g@hUYE{_$~}+X%*nytkW15']);
